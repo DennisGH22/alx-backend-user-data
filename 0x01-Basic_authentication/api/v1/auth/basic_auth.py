30f97702f5b4e83b609dc5dcb274d79d30a369d5
Tebogo Ndaba <dennistebogo@gmail.com>
2024-04-17 17:59:51 +0200

    Pycodestyle compliant

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,17 +9,24 @@ class BasicAuth(Auth):
     """
     Basic authentication class
     """
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
         """
-        Extracts the Base64 part of the Authorization header for Basic Authentication.
+        Extracts the Base64 part of the Authorization header
+        for Basic Authentication.
 
         Args:
             authorization_header (str): The Authorization header value.
 
         Returns:
-            str: The Base64 part of the Authorization header if present, otherwise None.
+            str: The Base64 part of the Authorization header if present,
+            otherwise None.
         """
-        if authorization_header is None or not isinstance(authorization_header, str):
+        if (
+            authorization_header is None or
+            not isinstance(authorization_header, str)
+        ):
             return None
 
         if not authorization_header.startswith('Basic '):

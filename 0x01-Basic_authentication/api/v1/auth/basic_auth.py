41d7d7b17407ed55dc5a83c9b3ae018bd9334587
Tebogo Ndaba <dennistebogo@gmail.com>
2024-04-17 19:10:04 +0200

    User object

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -109,7 +109,7 @@ class BasicAuth(Auth):
         if user_pwd is None or not isinstance(user_pwd, str):
             return None
 
-        users = User.search(email=user_email)
+        users = User.search({'email': user_email})
         if not users:
             return None
 

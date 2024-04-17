3ff3cd83e6ef05e2ed33c7ac633f2c48bd995ac4
Tebogo Ndaba <dennistebogo@gmail.com>
2024-04-18 00:32:49 +0200

    Add the URL path /api/v1/auth_session/login/ in the list of excluded paths

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -70,9 +70,6 @@ def before_request():
     ):
         abort(401)
 
-    if auth.authorization_header(request) is None:
-        abort(401)
-
     if auth.current_user(request) is None:
         abort(403)
 

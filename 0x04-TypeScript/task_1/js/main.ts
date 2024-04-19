523150454f7358f01d32e5064fa08ed02dd9ef9a
Tebogo Ndaba <dennistebogo@gmail.com>
2024-04-19 10:34:35 +0200

    Print the first letter of the firstName and the full lastName

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -69,3 +69,15 @@ const director1: Directors = {
 	numberOfReports: 17,
 };
 console.log(director1);
+
+interface PrintTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+
+const printTeacher: PrintTeacherFunction = (firstName: string, lastName: string): string => {
+    const firstInitial = firstName.charAt(0).toUpperCase();
+    const fullLastName = lastName.charAt(0).toUpperCase() + lastName.slice(1);
+    return `${firstInitial}. ${fullLastName}`;
+};
+
+console.log(printTeacher("John", "Doe"));

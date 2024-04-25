6fb8fc22e6588a840abcbf11d33cb7ed5aa93743
Tebogo Ndaba <dennistebogo@gmail.com>
2024-04-25 23:55:43 +0200

    Find user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,7 +5,16 @@ DB module
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError, NoResultFound
 from user import Base, User
+
+
+DB_COLUMNS = {'id',
+              'email',
+              'hashed_password',
+              'session_id',
+              'reset_token'
+              }
 
 
 class DB:
@@ -42,3 +51,27 @@ class DB:
         self._session.flush()
         self._session.commit()
         return user
+
+    def find_user_by(self, **criteria) -> User:
+        """
+        Find a user by the given criteria
+
+        Args:
+            criteria: Keyword argument representing the criteria to search for
+
+        Returns:
+            User: The found User object
+
+        Raises:
+            InvalidRequestError: If no valid criteria provided
+            NoResultFound: If no user is found matching the criteria
+        """
+        if not criteria or any(
+            field not in DB_COLUMNS for field in criteria
+        ):
+            raise InvalidRequestError
+        session = self._session
+        try:
+            return session.query(User).filter_by(**criteria).one()
+        except NoResultFound:
+            raise NoResultFound

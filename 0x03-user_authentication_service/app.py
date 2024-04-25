f20cfabc418f6713f8d70300d609708810d38f5e
Tebogo Ndaba <dennistebogo@gmail.com>
2024-04-26 00:57:31 +0200

    Implement the POST /users route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,15 +2,32 @@
 """
 Flask App
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/")
 def index():
     """ Route to return a welcome message. """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"])
+def register_user():
+    """ Endpoint to register a new user. """
+    try:
+        email = request.form["email"]
+        password = request.form["password"]
+        new_user = AUTH.register_user(email, password)
+        return jsonify({
+            "email": new_user.email,
+            "message": "user created"
+        }), 200
+    except ValueError as e:
+        return jsonify({"message": str(e)}), 400
 
 
 if __name__ == "__main__":
